4ed149d119e6403b0dd002d4dd8a96fa317cd102
Raghu Betina <raghu@firstdraft.com>
2016-11-16 00:33:11 +0000

    Validate presence of movies.duration

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,6 +5,8 @@ class Movie < ApplicationRecord
 
   # Validations
 
+  validates :duration, :presence => true
+
   validates :duration, :numericality => { :less_than_or_equal_to => 2764800, :greater_than_or_equal_to => 0 }
 
   validates :year, :numericality => { :less_than_or_equal_to => 2050, :greater_than_or_equal_to => 1870 }
