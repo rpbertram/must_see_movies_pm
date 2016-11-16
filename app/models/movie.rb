421ab543117a8d5cb38173c85b64ae14e5ead5b1
Raghu Betina <raghu@firstdraft.com>
2016-11-16 00:33:11 +0000

    Validate uniqueness of movies.duration

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,6 +5,8 @@ class Movie < ApplicationRecord
 
   # Validations
 
+  validates :duration, :uniqueness => { :scope => [:year] }
+
   validates :duration, :presence => true
 
   validates :duration, :numericality => { :less_than_or_equal_to => 2764800, :greater_than_or_equal_to => 0 }
